f5a7dede353838b59fb33ecbacc060ccb637893b
Andrew Grimberg <tykeal@bardicgrove.org>
2025-07-18 12:37:08 -0700

    Chore: Initial commit for GitHub setup and sources
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

---
+++ src/pyakuvox/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2023 Andrew Grimberg <tykeal@bardicgrove.org>
+"""pyakuvox: A Python library for interacting with the Akuvox VoIP system."""
+
+
+def hello() -> str:
+    """Your basic hello world function."""
+    return "Hello from pyakuvox!"
